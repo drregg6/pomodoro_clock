f1902ed7de0e3e4160b5da495ea4fa988c146f40
Dave <drregg6@gmail.com>
2017-07-21 12:41:29 -0400

    some crappy eventlisteners added

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,72 @@
-var buttons = document.getElementsByTagName('button');
+var sessionTimeAmount = document.querySelectorAll('.session-time-amount');
+var breakTimeAmount = document.querySelector('.break-time-amount');
+
+var intSessionTimeAmount = parseInt(sessionTimeAmount[0].textContent);
+var intBreakTimeAmount = parseInt(breakTimeAmount.textContent);
+
+var buttons = document.querySelectorAll('button');
 var startButton = buttons[4];
 var pauseButton = buttons[5];
+var lessSessionTime = buttons[0];
+var lessBreakTime = buttons[2];
+var moreSessionTime = buttons[1];
+var moreBreakTime = buttons[3];
+
+
+
+
+lessSessionTime.addEventListener('click', function(ev) {
+    if (intSessionTimeAmount > 0) {
+        intSessionTimeAmount--;
+        
+        sessionTimeAmount.forEach(function(sessionTime) {
+            sessionTime.textContent = '' + intSessionTimeAmount;
+        });
+    } else {
+        intSessionTimeAmount = 0;
+        sessionTimeAmount.forEach(function(sessionTime) {
+            sessionTime.textContent = '' + intSessionTimeAmount;
+        });
+    }
+});
+lessBreakTime.addEventListener('click', function() {
+    if (intBreakTimeAmount > 0) {
+        intBreakTimeAmount--;
+        
+        breakTimeAmount.textContent = '' + intBreakTimeAmount;
+    } else {
+        intBreakTimeAmount = 0;
+        
+        breakTimeAmount.textContent = '' + intBreakTimeAmount;
+    }
+});
+moreSessionTime.addEventListener('click', function() {
+    if (intSessionTimeAmount > -1 && intSessionTimeAmount < 60) {
+        intSessionTimeAmount++;
+        
+        sessionTimeAmount.forEach(function(sessionTime) {
+            sessionTime.textContent = '' + intSessionTimeAmount;
+        });
+    } else if (intSessionTimeAmount === 60) {
+        intSessionTimeAmount = 60;
+        
+        sessionTimeAmount.forEach(function(sessionTime) {
+            sessionTime.textContent = '' + intSessionTimeAmount;
+        });
+    }
+});
+moreBreakTime.addEventListener('click', function() {
+    if (intBreakTimeAmount > -1 && intBreakTimeAmount < 15) {
+        intBreakTimeAmount++;
+        
+        breakTimeAmount.textContent = '' + intBreakTimeAmount;
+    } else {
+        intBreakTimeAmount = 15;
+        
+        breakTimeAmount.textContent = '' + intBreakTimeAmount;
+    }
+});
+
 
 startButton.addEventListener('click', function() {
     this.classList.add('hidden');
@@ -11,5 +77,3 @@ pauseButton.addEventListener('click', function() {
     this.classList.add('hidden');
     startButton.classList.remove('hidden');
 });
-
-console.log('Hello world!');
